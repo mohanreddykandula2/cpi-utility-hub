eea494e8183a17fa27a94fc6bfca54ff4b7280a0
mohanDev <kandulamohanreddyking@gmail.com>
2026-05-24 22:43:51 +0530

    Remove generated and test artifacts

--- app/cpiutilityui/dist/controller/App.controller.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],i=>{"use strict";return i.extend("com.cpi.utility.cpiutilityui.controller.App",{onInit(){}})});
-//# sourceMappingURL=App.controller.js.map
